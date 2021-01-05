ea9a88b69a44c017b1f200ac49bdb80b894f8a2e
saram sw <saramsoftware@gmail.com>
2021-01-05 21:28:19 +0900

    add app id and ad unit id

--- lib/util/ads/ad_info.dart
+++ lib/util/ads/ad_info.dart
@@ -1,4 +1,4 @@
 class AdInfo{
   // You must enter your ad unit id
-  static final unitIdBannerBottom = "ca-app-pub-";
+  static final unitIdBannerBottom = "ca-app-pub-3393411258858656/2707331884";
 }
